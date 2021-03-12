f2f9375a9d933d48976485bf36f70326996ff51e
Ethan Borrowman <ethan_borrowman@hotmail.com>
2021-03-11 21:02:16 -0500

    Finishing create, edit, and delete core data methods

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     private var models = [ToDoListItem]()
     
     let tableView: UITableView = {
@@ -20,8 +20,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         title = "CoreData To Do List"
+        getAllItems()
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
@@ -40,7 +40,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
             self?.createItem(name: text)
         }))
-        present(alert, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+        present(alert, animated: true)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,10 +54,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit Item", message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { [weak self] _ in
+            let alert = UIAlertController(title: "Edit Item", message: "Edit Your Item", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: text)
+            }))
+            self?.present(alert, animated: true)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        
+        present(sheet, animated: true)
+    }
+    
     // Core Data
     func getAllItems() {
         do {
-            models = try context.fetch(ToDoListItem.fetchRequest())
+            guard let appContext = context else { return }
+            
+            models = try appContext.fetch(ToDoListItem.fetchRequest())
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -68,12 +97,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func createItem(name: String) {
-        let newItem = ToDoListItem(context: context)
+        guard let appContext = context else { return }
+        
+        let newItem = ToDoListItem(context: appContext)
         newItem.name = name
         newItem.createdAt = Date()
         
         do {
-            try context.save()
+            try appContext.save()
+            getAllItems()
         }
         catch {
             
@@ -81,20 +113,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func deleteItem(item: ToDoListItem) {
-        context.delete(item)
+        guard let appContext = context else { return }
+        appContext.delete(item)
         
         do {
-            try context.save()
+            try appContext.save()
+            getAllItems()
         }
         catch {
         }
     }
     
     func updateItem(item: ToDoListItem, newName: String) {
+        guard let appContext = context else { return }
         item.name = newName
         
         do {
-            try context.save()
+            try appContext.save()
+            getAllItems()
         }
         catch {
         }
